339c139248851172521d260b9755613282480a9f
Matt Karl <matt@mattkarl.com>
2022-07-14 17:21:28 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Application, Sprite, Assets } = require('@pixi/node');
+const path = require('path');
+
+(async () => {
+    await Assets.init();
+    const app = new Application();
+    
+    // load a sprite
+    const bunnyTexture = await Assets.load(path.join(__dirname, 'assets/bunny.png'));
+    const bunny = Sprite.from(bunnyTexture);
+    
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+    
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+    
+    // Add the bunny to the scene we are building.
+    app.stage.addChild(bunny);
+    
+    // Listen for frame updates
+    app.ticker.add(() => {
+            // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+    });
+    
+    // extract and save the stage
+    app.renderer.render(stage);
+    const base64Image = app.renderer.plugins.extract
+        .canvas(stage)
+        .toDataURL('image/png');
+    
+    const base64Data = base64Image.replace(/^data:image\/png;base64,/, '');
+    const output = `./test.png`;
+    
+    writeFileSync(output, base64Data, 'base64');
+
+})();
